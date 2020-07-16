5f99030bd1af153aec5d7d294dfbf6a41ebf073f
khucklanger <khucklanger@freenet.de>
2020-07-16 16:30:44 +0200

    Merge pull request #1 from lithgow/master
    gradle build and upload test + coverage results

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -8,11 +8,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class AccountTest {
 
     Account objA = new Account();
+    int b = 10;
 
     @Test
     public void getbalance () {assertThat(objA.bal=0);}
-  //  @Test
-  //  public void setbalance () {assertThat(objA.setBalance(10)=10);}
+    @Test
+    public void setbalance () {assertThat(objA.setBalance(10)==10);}
 	@Disabled
     public void depositAnAmountToIncreaseTheBalance() {
         assertThat("your first test").isBlank();
